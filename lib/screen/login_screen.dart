dec7afda01e5a56affaf4993ebdf9e304c2240ae
Saurav <deveneoxys@gmail.com>
2023-11-17 15:46:09 +0530

    16/11/2023

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -52,9 +52,7 @@ class _LoginScreenState extends State<LoginScreen> {
         .get();
     log(result.docs.toString());
     if (result.docs.isNotEmpty) {
-      print(result.docs.first.data());
       Map kk = result.docs.first.data() as Map;
-      print(kk["email"]);
       login(kk["email"].toString());
     } else {
       Fluttertoast.showToast(msg: 'Phone Number not register yet Please Signup');
@@ -62,9 +60,6 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   login(String email) async {
-    await FirebaseAuth.instance.signOut();
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email, password: "123456");
     OverlayEntry loader = Helper.overlayLoader(context);
     Overlay.of(context).insert(loader);
     try {
@@ -83,6 +78,7 @@ class _LoginScreenState extends State<LoginScreen> {
           Get.to(() => OtpScreen(
                 verificationId: verificationId,
                 code: code,
+            email: email,
               ));
         },
         codeAutoRetrievalTimeout: (String verificationId) {
